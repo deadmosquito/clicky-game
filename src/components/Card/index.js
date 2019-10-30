b5a41834432a368e82165491216bb4f951efe816
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-10-29 17:22:44 -0700

    update cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,30 +1,13 @@
+
 import React from "react";
 import "./style.css";
 
-function Card(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
+const Card = props => (
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
     </div>
-  );
-}
+  </div>
+);
 
 export default Card;
