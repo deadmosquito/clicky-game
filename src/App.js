aceacbe40af0004f3a7a638c16e666c69c8f136d
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-10-29 17:49:17 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
     highscore: 0
   };
 
-  
+  // this is run once a card is clicked more than once
   gameOver = () => {
     if (this.state.score > this.state.highscore) {
       this.setState({highscore: this.state.score}, function() {
-        console.log(this.state.highscore);
       });
     }
+    //setting the initial to zero
     this.state.cards.forEach(card => {
       card.count = 0;
     });
@@ -26,14 +26,13 @@ class App extends Component {
     this.setState({score: 0});
     return true;
   }
-
+// this is to identify if the card has been clicked once which is okay else run the gameover
   clickCount = id => {
     this.state.cards.find((x, i) => {
       if (x.id === id) {
         if(cards[i].count === 0){
           cards[i].count = cards[i].count + 1;
           this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
           });
           this.state.cards.sort(() => Math.random() - 0.5)
           return true; 
@@ -48,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>Game of Thrones Clicky Game</Header>
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
